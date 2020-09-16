6a4de11f9e05bb1313ab97001567d6560a055501
yogesh kumar <5608439-yogesh_15@users.noreply.gitlab.com>
2020-09-16 16:40:54 +0530

    first commit

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express();
 app.use(express.static(__dirname + '/dist/heroku'));
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname +
-        '/dist/myapp/index.html'));
+        '/dist/heroku/index.html'));
 });
 app.listen(process.env.PORT || 8080);
